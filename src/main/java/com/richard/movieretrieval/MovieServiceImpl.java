de994cc13963374932e7d35b50dee2f8a9e19b05
rjohn <richard.john@andrena.de>
2018-02-18 13:42:56 +0100

    update dependency commons-lang

--- src/main/java/com/richard/movieretrieval/MovieServiceImpl.java
+++ src/main/java/com/richard/movieretrieval/MovieServiceImpl.java
@@ -3,7 +3,7 @@ package com.richard.movieretrieval;
 import java.util.List;
 import java.util.Optional;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 import com.richard.model.Movie;
